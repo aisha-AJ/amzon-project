98153a99d3721cdfd4b85282b47f54bfe13cd9d3
Asha Osman <asha3osman@gmail.com>
2026-01-05 16:00:30 +0300

    make cart quantity inter active

--- javascript-amazon-project-main/data/Amazon.js
+++ javascript-amazon-project-main/data/Amazon.js
@@ -64,29 +64,39 @@ innerHTML = productsHTML;
 
 
 document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-      button.addEventListener('click', () => {
-        const productId = button.dataset.
-        productId;
+.forEach((button) => {
+  button.addEventListener('click', () => {
+   const productId = button.dataset.
+   productId;
 
-        let matchingItem;
+   let matchingItem;
 
-        cart.forEach((item) => {
-            if(productId === item.productId) {
-                matchingItem = item;
-            }
-        });
+   cart.forEach((item) => {
+      if(productId === item.productId) {
+        matchingItem = item;
+      }
+    });
 
-        if(matchingItem) {
-          matchingItem.quantity += 1;
-        }else{
-          cart.push({
-          productId: productId,
-          quantity: 1
-        });
-        }
+    if(matchingItem) {
+      matchingItem.quantity += 1;
+    }else{
+     cart.push({
+      productId: productId,
+      quantity: 1
+     });
+
+    }
+    let cartQuantity = 0;
+
+
+    cart.forEach( (item) => {
+      cartQuantity += item.quantity
+
+    });
+
+    document.querySelector('.js-cart-quantity').
+    innerHTML = cartQuantity;
         
-        
-        console.log(cart);
-      });
+    
   });
+});
