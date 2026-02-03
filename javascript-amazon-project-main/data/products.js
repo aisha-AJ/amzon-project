81befe323cb4716c2d69416297e67463fa255de3
Asha Osman <asha3osman@gmail.com>
2026-02-03 10:34:31 +0300

    convert loadProducts from XHR to fetch

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -65,6 +65,30 @@ import { formatCurrency } from "../script/utils/money.js";
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+   return response.json()
+  }).then((productData) => {
+     
+    products = productData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+
+      return new product(productDetails);
+    });
+    console.log('load Products');
+    
+ 
+  });
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('nex step');
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest(); 
 
